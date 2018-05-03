7c261d345e688939cc6454077e0bc52accb3bce9
janewuwz <janewuwz@gmail.com>
2018-05-03 16:18:57 +0800

    1 case

--- index.ts
+++ index.ts
@@ -1,8 +1,18 @@
 class Observable {
+  // 如果在一个方法前，加上static关键字，就表示该方法不会被实例继承，而是直接通过类来调用，这就称为“静态方法”
   static create(fn: Function) {
-    fn()
+    var observers:any = []
+    interface inte {
+      next: any,
+      error: any,
+      complete: any
+    }
+    return {
+      subscribe: function(obj: any|inte){
+        fn(obj)
+      }
+    }
   }
-  subscribe(){}
 }
 
 class Subject {
--- test.ts
+++ test.ts
@@ -2,6 +2,13 @@ import 'mocha';
 import Rx from './index';
 import { expect } from 'chai';
 
+// case 1
+// VM496:13 got value 1
+// VM496:13 got value 2
+// VM496:13 got value 3
+// undefined
+// VM496:13 got value 4
+// VM496:15 done
 var observable = Rx.Observable.create(function (observer: any) {
   observer.next(1);
   observer.next(2);
@@ -11,9 +18,11 @@ var observable = Rx.Observable.create(function (observer: any) {
     observer.complete();
   }, 1000);
 });
-
+console.log('just before subscribe');
 observable.subscribe({
   next: (x: any) => console.log('got value ' + x),
   error: (err: any) => console.error('something wrong occurred: ' + err),
   complete: () => console.log('done'),
 });
+console.log('just after subscribe');
+
